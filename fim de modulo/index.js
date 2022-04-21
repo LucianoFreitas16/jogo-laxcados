8122f471bb71492c0b11b73af4fad1f101d9f336
Luciano Freitas <102334190+LucianoFreitas16@users.noreply.github.com>
2022-04-21 00:51:19 -0300

    Delete fim de modulo directory

--- fim de modulo/index.js
+++
@@ -1,9 +0,0 @@
-var botao = document.getElementById("btn");
-botao.addEventListener('click',function()
-{
- var mostraMsg=prompt=("Será que nossos laxcados irão se sair bem?")
-    if (mostraMsg.toLowerCase() == "sim")
-    { return location="./resumo.html"
-
-    }
-})
