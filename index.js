cb1de097fe7b9e5f8df4fcdd49000235927d852b
Luciano Freitas <lucianoft16@outlook.com>
2022-04-22 01:23:38 -0300

    erro no js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var botao = document.getElementById("btn");
 botao.addEventListener('click', function () {
     var mostraMsg = prompt = ("Será que nossos laxcados irão se sair bem?")
     if (mostraMsg.toLowerCase() == "sim") {
-        return window.location = "./resumo.html"
+        return location = "./resumo.html"
 
     }
 })
