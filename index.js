88200368fa5c0f7969f0e5b8ee604b508896aa61
Luciano Freitas <lucianoft16@outlook.com>
2022-04-22 01:35:07 -0300

    erro no index

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var botao = document.getElementById("btn");
-botao.addEventListener('click', function () {
-    var mostraMsg = prompt = ("Será que nossos laxcados irão se sair bem?")
+botao.addEventListener('click', function (mostraMsg) {
+    mostraMsg = prompt("Você quer jogar?\nDigite sim ou não")
     if (mostraMsg.toLowerCase() == "sim") {
-        return location = "./resumo.html"
-
+        alert("Você deve tomar decisões sobre a situação que o personagem escolhido se encontra, informando a sua opção de escolha através dos prompts na tela, digitando sim ou não.\nSerá que nossos laxcados se sairão bem?")
+        return location = "./escolhaDePersonagem.html"
+    } else if (mostraMsg.toLowerCase() == "não" || mostraMsg.toLowerCase() == "nao") {
+        alert("Parabéns você perdeu a oportunidade de salvar um laxcado!")
+    } else {
+        alert("Essa opção não foi encontrada, tente novamente.")
     }
 })
