b7ced4323ffe87fb610ad2e9abdd965fdc70f23d
Luciano Freitas <lucianoft16@outlook.com>
2022-04-22 00:36:56 -0300

    mudança no js

---
+++ index.js
@@ -0,0 +1,9 @@
+var botao = document.getElementById("btn");
+botao.addEventListener('click',function()
+{
+ var mostraMsg=prompt=("Será que nossos laxcados irão se sair bem?")
+    if (mostraMsg.toLowerCase() == "sim")
+    { return window.location="./resumo.html"
+
+    }
+})
