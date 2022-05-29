6f92ae3e7baf130560f9b2402f4b1ff787f31d83
Luciano Freitas <lucianoft16@outlook.com>
2022-05-28 23:18:46 -0300

    criação de pagina vitoria e derrota

---
+++ vitoria.js
@@ -0,0 +1,5 @@
+var buttonV = document.getElementById("win");
+buttonV.addEventListener('click', function (voltaInicioV) {
+    voltaInicioV = alert("Obrigado por jogar nosso jogo!")
+    return location = "./index.html"
+})
